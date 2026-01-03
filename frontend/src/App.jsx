517220723e6b3c7dbeb40accb9136a284445f693
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2026-01-03 15:29:14 +0530

    feat: initialize frontend with React, Vite, and ESLint
    - Added package.json for project dependencies and scripts
    - Created App.css for styling the application
    - Implemented main App component with routing and state management for todos
    - Added global styles in index.css
    - Set up main entry point in main.jsx with React Router
    - Configured Vite for the project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "./App.css";
+
+import Todos from "./components/Todos";
+import { TodoForm } from "./components/TodoForm";
+
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  // const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
+
+  // console.log(user);
+
+  return (
+    <>
+      <div className="container">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div className="main">
+                <TodoForm todos={todos} setTodos={setTodos} />
+                <Todos todos={todos} setTodos={setTodos} />
+              </div>
+            }
+          ></Route>
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default App;
