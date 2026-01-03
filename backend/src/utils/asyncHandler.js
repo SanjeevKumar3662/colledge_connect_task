0b039ad37c821d02b9eb8f448f29c7db81138c6c
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2026-01-03 14:55:02 +0530

    fix: correct export statement for asyncHandler utility

--- backend/src/utils/asyncHandler.js
+++ backend/src/utils/asyncHandler.js
@@ -1,7 +1,9 @@
-export const asyncHandler = (fn) => async (req, res, next) => {
+const asyncHandler = (fn) => async (req, res, next) => {
   try {
     await fn(req, res, next);
   } catch (error) {
     next(error);
   }
 };
+
+export default asyncHandler;
